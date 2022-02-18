0dbb4653f5e0a4efcf5b5cda2004859e2c91b14a
matthewsorbara <matthewsorbara01@gmail.com>
2022-02-18 16:09:41 +1100

    add optimal solution for mountain

--- Optimal & Neetcode Solutions/mountainArray.py
+++ Optimal & Neetcode Solutions/mountainArray.py
@@ -10,4 +10,24 @@ def linearScan(arr):
     return i-1
 
 
+
+def binaryScan(arr):
+    # optimal solution, O(log n) time complexity
+
+
+    left, right = 0, len(arr)-1
+        
+    while left < right:
+        
+        mid = left + ((right - left) // 2)
+        
+        if arr[mid] < arr[mid+1]:
+            left = mid+1
+        
+        else:
+            right = mid
+        
+    return left
+
 print(linearScan(sampleMountain))
+print(binaryScan(sampleMountain))
