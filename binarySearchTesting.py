8b8dcce7f2a822ed04a8561d1861c021dcbf9e51
matthewsorbara <matthewsorbara01@gmail.com>
2022-02-20 17:07:52 +1100

    random changes

--- binarySearchTesting.py
+++ binarySearchTesting.py
@@ -1,3 +1,4 @@
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -67,12 +68,69 @@ class PostOrderSolution:
             self.traverse(root, values)
             
         return values
+
+
+
+class SymmetrySolution:
+    def leftSideSymmetry(self, node, leftValues):
+        if node == None: 
+            leftValues.append(None)
+            return
+        
+        leftValues.append(node.val)
+        
+        self.leftSideSymmetry(node.left, leftValues)
+        self.leftSideSymmetry(node.right, leftValues)
+            
+        if node.left == None and node.right == None:
+            return
+        
+        
+    def rightSideSymmetry(self, node, rightValues):
+        if node == None: 
+            rightValues.append(None)
+            return
+        
+        rightValues.append(node.val)
+    
+        self.rightSideSymmetry(node.right, rightValues)
+        self.rightSideSymmetry(node.left, rightValues)
+    
+        if node.left == None and node.right == None:
+            return
+        
+    
+    def isSymmetric(self, root):
+        
+        leftValues = []
+        rightValues = []
+        
+        self.leftSideSymmetry(root, leftValues)
+        self.rightSideSymmetry(root, rightValues)
+        
+        print(leftValues)
+        print(rightValues)
+
+        if leftValues == rightValues:
+            return True
+        else:
+            return False
+
+
+
+
+symmetry = SymmetrySolution()
+
+root = TreeNode(1, TreeNode(2, None, TreeNode(3)),TreeNode(2,None, TreeNode(3)))
+
+print(symmetry.isSymmetric(root))
+    
         
 
-pre_order = PreOrderSolution()
-in_order = InOrderSolution()
-post_order = PostOrderSolution()
+# pre_order = PreOrderSolution()
+# in_order = InOrderSolution()
+# post_order = PostOrderSolution()
 
-print(pre_order.preorderTraversal(root))    
-print(in_order.inOrderTraversal(root))
-print(post_order.postorderTraversal(root))
+# print(pre_order.preorderTraversal(root))    
+# print(in_order.inOrderTraversal(root))
+# print(post_order.postorderTraversal(root))
