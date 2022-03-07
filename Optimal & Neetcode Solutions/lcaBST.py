b94e33eab1b9e9e879997eb7204bb21a5589b400
Matt <50189255+mattsob123@users.noreply.github.com>
2022-03-07 20:19:52 +1100

    lowest common ancestor bst

---
+++ Optimal & Neetcode Solutions/lcaBST.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def lowestCommonAncestor(self, root, p, q):
+        curr = root
+
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+
+            else:
+                return curr
